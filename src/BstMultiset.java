4edd49e3742ac3e30bfdd1306247e0982e7c1e04
Chris Chu <c20711@gmail.com>
2017-08-26 15:34:50 +1000

    BST final update

--- src/BstMultiset.java
+++ src/BstMultiset.java
@@ -1,85 +1,222 @@
 import java.io.PrintStream;
 import java.util.*;
 
-public class BstMultiset<T> extends Multiset<T> {
-
-	Node<T> parent;
-	Node<T> root;
-	int size;
+public class BstMultiset<T extends Comparable<T>> extends Multiset<T> {
+	protected Node mRoot;
 
 	public BstMultiset() {
-		parent = null;
-		size = 0;
+		mRoot = null;
 	} // end of BstMultiset()
 
 	public void add(T item) {
-		Node current = new Node(item);
-
-		/* The first element */
-		if (root == null) {
-			root = current;
-			root.num = 1;
-			return;
-
-		}
-		parent = root;
-		while (true) {
-			if (parent.value.toString().compareTo(current.value.toString()) == 0) {
-				parent.num++;
-			} else if (parent.value.toString().compareTo(current.value.toString()) < 0) {
-				if(parent.left == null)
-					parent.left = current;
-				else
-				parent = parent.left;
-				return;
-				
-			} else if (parent.value.toString().compareTo(current.value.toString()) > 0) {
-				if(parent.right == null)
-					parent.right = current;
-				else
-				parent = parent.right;
-				return;
+		Node newNode = new Node(item, 1);
+		newNode.setLchild(null);
+		newNode.setRchild(null);
+		// If heap is empty, then put element as the root node.
+		if (mRoot == null) {
+			mRoot = newNode;
+		} else {
+			Node currNode = mRoot;
+			while (currNode != null) {
+				if (item.compareTo(currNode.getValue()) == 0) {
+					// If node has some value as input item
+					currNode.increaseCount();
+					break;
+				} else if (item.compareTo(currNode.getValue()) > 0) {
+					// if item is greater than the value of current node
+					if (currNode.getRchild() != null) {
+						currNode = currNode.getRchild();
+					} else {
+						currNode.setRchild(newNode);
+						break;
+					}
+				} else {
+					// If item is less than the value of current node
+					if (currNode.getLchild() != null)
+						currNode = currNode.getLchild();
+					else {
+						currNode.setLchild(newNode);
+						break;
+					}
+				}
 			}
 		}
-
 	} // end of add()
 
 	public int search(T item) {
-		// Implement me!
-
-		// default return, please override when you implement this method
+		Node currNode = mRoot;
+		while (currNode != null) {
+			if (item.compareTo(currNode.getValue()) == 0) {
+				// return the count of the given item
+				return currNode.getCount();
+			} else if (item.compareTo(currNode.getValue()) > 0) {
+				// if item is greater than the value of current node
+				if (currNode.getRchild() != null)
+					currNode = currNode.getRchild();
+			} else {
+				// If item is less than the value of current node
+				if (currNode.getLchild() != null)
+					currNode = currNode.getLchild();
+			}
+		}
+		// End of while loop, and item is not found in the heap
 		return 0;
-	} // end of add()
+	} // end of search()
+
+	private Node delete(Node tnode, T item) {
+		int identical = item.compareTo(tnode.getValue());
+		if (identical > 0)
+			tnode.setRchild(delete(tnode.getRchild(), item));
+		else if (identical < 0)
+			tnode.setLchild(delete(tnode.getLchild(), item));
+		else {
+			if (tnode.getCount() > 1) {
+				tnode.decreaseCount();
+				return tnode;
+			}
+			if (tnode.getRchild() == null) {
+				// If target node has no R child then take L child to replace
+				// target node
+				return tnode.getLchild();
+			}
+			if (tnode.getLchild() == null) {
+				// If target node has no L child then take R child to replace
+				// target node
+				return tnode.getRchild();
+			}
+
+			Node temp = tnode;
+			tnode = getMin(temp.getRchild());
+
+			tnode.setRchild(deleteMin(temp.getRchild()));
+			tnode.setLchild(temp.getLchild());
+		}
+		return tnode;
+	}
+
+	private Node deleteMin(Node tnode) {
+		if (tnode.getLchild() == null)
+			return tnode.getRchild();
+		tnode.setLchild(deleteMin(tnode.getLchild()));
+		return tnode;
+	}
 
 	public void removeOne(T item) {
-		// Implement me!
+		mRoot = delete(mRoot, item);
+
 	} // end of removeOne()
 
 	public void removeAll(T item) {
-		// Implement me!
+		int count = mRoot.mCount;
+		for (int i = 0; i<count ; i++)
+		mRoot = delete(mRoot, item);
 	} // end of removeAll()
 
 	public void print(PrintStream out) {
-		// Implement me!
+		// Use stack structure to store nodes
+		Stack<Node> stack = new Stack<Node>();
+		Node currNode = mRoot;
+
+		if (currNode == null) {
+			return;
+		}
+		while (!stack.empty() || currNode != null) { // Step through whole list
+														// until stack is empty
+
+			//      6
+			//    /   \
+			//   3     8
+			//  /       \
+			// 2         4
+			//
+			// The traversal will be 2->3->4->6->8
+
+			if (currNode != null) {
+				// Traverse until hit the most left child and push every lchild
+				// into stack
+				stack.push(currNode);
+				currNode = currNode.getLchild();
+			} else {
+				currNode = stack.pop();
+				out.println(currNode.getValue() + printDelim + currNode.getCount());
+				currNode = currNode.getRchild();
+			}
+		}
 	} // end of print()
 
-	class Node<T> implements Comparable<T> {
-		public int num;
-		public T value;
-		public Node<T> left;
-		public Node<T> right;
-
-		Node(T value) {
-			num = 0;
-			this.value = value;
-			this.right = null;
-			this.left = null;
+	private Node getMin(Node root) {
+		Node currNode = root;
+		while (currNode.getLchild() != null) {
+			currNode = currNode.getLchild();
 		}
 
-		@Override
-		public int compareTo(T value) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+		return currNode;
 	}
+
+	/**
+	 * Node type, inner class.
+	 */
+	class Node {
+		/** Stored value of node. */
+		protected int mCount;
+		protected T mItem;
+		/** Reference to next node. */
+		protected Node lChild;
+		protected Node rChild;
+
+		public Node(T item, int count) {
+			mItem = item;
+			mCount = count;
+			lChild = null;
+			rChild = null;
+		}
+
+		public T getValue() {
+			return mItem;
+		}
+
+		public Node getLchild() {
+			return lChild;
+		}
+
+		public Node getRchild() {
+			return rChild;
+		}
+
+		public void setValue(T item) {
+			mItem = item;
+		}
+
+		public void setLchild(Node tnode) {
+			lChild = tnode;
+		}
+
+		public void setRchild(Node tnode) {
+			rChild = tnode;
+		}
+
+		public int getCount() {
+			return mCount;
+		}
+
+		public void setCount(int count) {
+			mCount = count;
+		}
+
+		public void increaseCount() {
+			mCount++;
+		}
+
+		public void decreaseCount() {
+			mCount--;
+		}
+
+		public boolean isLeaf() {
+			if (lChild == null && rChild == null)
+				return true;
+			return false;
+		}
+
+	} // end of inner class Node
+
 } // end of class BstMultiset
